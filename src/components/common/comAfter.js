614a8a0f33a62b2cf40bcd93c460c105ff256149
wangyang <i@easybe.org>
2024-06-14 17:02:46 +0800

    refactor(comAfter): comafter

--- src/components/common/comAfter.js
+++ src/components/common/comAfter.js
@@ -6,92 +6,55 @@
  * ----------------------------------------------
  * @describe: 后置公共处理
  */
-import progress from "../progress/progress";
-import title from "../title/title";
-import footer from "../footer/footer";
-import rtMenu from "../rtMenu/rtMenu";
-import blogIcon from "../blogIcon/blogIcon";
-import dayNight from "../dayNight/dayNight";
-import console from "../console/console";
+import progress from '../progress/progress';
+import title from '../title/title';
+import footer from '../footer/footer';
+import rtMenu from '../rtMenu/rtMenu';
+import blogIcon from '../blogIcon/blogIcon';
+import dayNight from '../dayNight/dayNight';
+import console from '../console/console';
 
 export default function main() {
+    // 页脚
+    footer();
 
-    /**
-     * 页脚
-     */
-    (() => {
-        footer();
-    })();
+    // 右下角菜单
+    rtMenu();
 
-    /**
-     * 右下角菜单
-     */
-    (() => {
-        rtMenu();
-    })();
+    // 日/夜模式
+    dayNight();
 
-    /**
-     * 日/夜模式
-     */
-    (() => {
-        dayNight();
-    })();
+    // 进度条
+    progress();
 
-    /**
-     * 进度条
-     */
-    (() => {
-        progress();
-    })();
-
-    /**
-     * 背景动效
-     */
-    (() =>  {
-        let obj = $.__config.animate.background;
-        for (let key in obj) {
-            if (obj[key].enable) {
-                import(/* webpackChunkName: "background-[request]" */ /* webpackPrefetch: true */ `../background/${key}`).then(module => {
-                    let background = module.default;
-                    background(obj[key]?.options);
-                });
+    // 背景动效
+    (async () => {
+        for (const [key, config] of Object.entries($.__config.animate.background)) {
+            if (config.enable) {
+                const module = await import(/* webpackChunkName: "background-[request]" */ `../background/${key}`);
+                const backgroundEffect = module.default;
+                backgroundEffect(config.options || {});
             }
         }
     })();
 
-    /**
-     * 鼠标动效
-     */
-    (() => {
-        let obj = $.__config.animate.mouse;
-        for (let key in obj) {
-            if (obj[key].enable) {
-                import(/* webpackChunkName: "mouse-[request]" */ /* webpackPrefetch: true */ `../mouse/${key}`).then(module => {
-                    let mouse = module.default;
-                    mouse(obj[key]?.options);
-                });
+    // 鼠标动效
+    (async () => {
+        for (const [key, config] of Object.entries($.__config.animate.mouse)) {
+            if (config.enable) {
+                const module = await import(/* webpackChunkName: "mouse-[request]" */ `../mouse/${key}`);
+                const mouse = module.default;
+                mouse(config.options);
             }
         }
     })();
 
-    /**
-     * 网站图标
-     */
-    (() => {
-        blogIcon();
-    })();
+    // 网站图标
+    blogIcon();
 
-    /**
-     * 页面title
-     */
-    (() => {
-        title();
-    })();
+    // 页面title
+    title();
 
-    /**
-     * 控制台输出
-     */
-    (() => {
-        console();
-    })();
+    // 控制台输出
+    console();
 }
