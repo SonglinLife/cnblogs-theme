fc14cabcec14b960d599a329fc4781cf214b0427
wangyang <i@easybe.org>
2024-06-14 12:58:25 +0800

    refactor(home): home

--- src/page/article.js
+++ src/page/article.js
@@ -1,11 +1,3 @@
-/**
- * UPDATES AND DOCS AT: https://github.com/wangyang0210
- * https://www.cnblogs.com/wangyang0210/
- * @author: WangYang, wangyang.0210@foxmail.com
- * @Date 2022-08-25 15:26
- * ----------------------------------------------
- * @describe: 文章页处理
- */
 import comArticle from './common/com-article';
 import imgBox from '../components/imgBox/imgBox';
 await $.__tools.dynamicLoadingJs($.__config.default.iconfont).catch((e) => console.error('iconfont.js', e));
@@ -14,7 +6,7 @@ export default function main() {
     // 文章页公共处理
     comArticle();
 
-    // 代码高亮处理 | 异步处理防止影响loading结束
+    // 代码高亮处理
     import(/* webpackChunkName: "article-code" */ /* webpackPrefetch: true */ '../components/code/code').then(
         (module) => {
             const code = module.default;
