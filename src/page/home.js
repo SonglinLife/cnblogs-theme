3eaa543d3853a7a7514a7871b856069493fba1f8
wangyang <i@oyo.cool>
2024-12-18 11:45:01 +0800

    chore(home): home

--- src/page/home.js
+++ src/page/home.js
@@ -20,51 +20,45 @@ export default function main() {
 
     // 主页的随机一言
     let hitokoto = $('#hitokoto');
-    const updateHitokotoDisplay = () => {
-        hitokoto.show().css('display', '-webkit-box');
+    let configTitle = $.__config.banner.home.title;
+    const topTitleList = ['当你凝视深渊时，深渊也在凝视着你。', '有的人25岁就死了，只是到75岁才埋葬'];
+
+    const updateHitokotoDisplay = (content) => {
+        hitokoto.html(content).css('display', '-webkit-box');
         $.__tools.setDomHomePosition();
     };
-    let configTitle = $.__config.banner.home.title;
-    if (Array.isArray(configTitle) && configTitle.length > 0) {
+
+    if (Array.isArray(configTitle) && configTitle.length) {
         let listIndex = $.__tools.randomNum(0, configTitle.length - 1);
-        hitokoto.html(configTitle[listIndex]);
-        updateHitokotoDisplay();
+        updateHitokotoDisplay(configTitle[listIndex]);
         return;
     }
     if (typeof configTitle === 'string' && configTitle !== '') {
-        hitokoto.html(configTitle);
-        updateHitokotoDisplay();
+        updateHitokotoDisplay(configTitle);
         return;
     }
-
-    const topTitleList = ['当你凝视深渊时，深渊也在凝视着你。', '有的人25岁就死了，只是到75岁才埋葬'];
 
     function fetchAndSetTitle(url) {
         request(url)
             .then(topTitleContent)
             .catch(() => {
                 let listIndex = $.__tools.randomNum(0, topTitleList.length - 1);
-                hitokoto.html(topTitleList[listIndex]);
-                updateHitokotoDisplay();
+                updateHitokotoDisplay(topTitleList[listIndex]);
             });
     }
 
     function topTitleContent(r) {
         if (r.status === 'success') {
-            let {
-                note = r.data.content,
-                data: {
-                    origin: { title, dynasty, author },
-                },
-            } = r;
-            hitokoto.html(note);
-            $('#hitokotoAuthor').text(`《${title}》 - ${dynasty} - ${author}`).show();
+            const note = r?.note || r.data.content;
+            const poetry = `《${r.data.origin.title}》 - ${r.data.origin.dynasty} - ${r.data.origin.author}`;
+            let content = r?.content || poetry;
+            updateHitokotoDisplay(note);
+            $('#hitokotoAuthor').text(content).show();
         }
-        updateHitokotoDisplay();
     }
 
     const titleSources = {
-        one: 'https://api.wangyangyang.vip/',
+        one: 'https://one.oyo.cool/',
         jinrishici: 'https://v2.jinrishici.com/one.json',
     };
 
@@ -72,8 +66,7 @@ export default function main() {
         fetchAndSetTitle(titleSources[$.__config.banner.home.titleSource]);
     } else {
         let listIndex = $.__tools.randomNum(0, topTitleList.length - 1);
-        hitokoto.html(topTitleList[listIndex]);
-        updateHitokotoDisplay();
+        updateHitokotoDisplay(topTitleList[listIndex]);
     }
 
     // 头图点击滚动到内容位置
@@ -88,7 +81,7 @@ export default function main() {
     $.each(allTitles, (i, titleElement) => {
         let title = $(titleElement),
             titleText = title.text(),
-            postDescText = title.nextAll('.postDesc, .entrylistItemPostDesc:eq(0)').text();
+            postDescText = title.nextAll('.postDesc:eq(0), .entrylistItemPostDesc:eq(0)').text();
         title.after(postMetaHtml(postDescText));
         if (title.hasClass('postTitle') && /\[置顶\]/.test(titleText)) {
             title.append('<span class="postSticky">置顶</span>');
@@ -113,15 +106,14 @@ export default function main() {
         .map((i) => {
             let obj = $(desc[i]),
                 img = obj.find('img.desc_img');
-            if (img.length > 0 && img.attr('src')) {
+            if (img.length && img.attr('src')) {
                 let src = img.attr('src');
                 img.hide();
-                obj.css('width', '60%');
+                obj.addClass('desc-width-60');
                 let parentDiv = obj.parent('div');
-                if (parentDiv.length > 0) {
-                    parentDiv.css('min-height', '150px');
-                    let html = `<div class="c_b_p_desc_img"><div class="background-image-holder" style="background-image: url('${src}')"></div></div>`;
-                    return html;
+                if (parentDiv.length) {
+                    parentDiv.addClass('desc-parent-minheight-150');
+                    return `<div class="c_b_p_desc_img"><div class="background-image-holder" style="background-image: url('${src}')"></div></div>`;
                 }
             }
             return '';
