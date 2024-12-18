e5ba8670e734974d365164891561ec8658631bef
wangyang <i@oyo.cool>
2024-12-18 09:11:35 +0800

    chore(package): update npm

--- src/page/books.js
+++ src/page/books.js
@@ -11,6 +11,21 @@ export default function main() {
         const postBody = $('#cnblogs_post_body');
         const articleSuffixFlg = $('.articleSuffix-flg');
         let html = '';
+        const templateData = {
+            formerName: '原　名',
+            author: '作　者',
+            translator: '译　者',
+            press: '出版社',
+            year: '出版年',
+            scenarist: '编　剧',
+            star: '主　演',
+            type: '类　型',
+            productionCountry: '制片国家/地区',
+            language: '语　言',
+            releaseDate: '上映日期',
+            filmLength: '片　长',
+            alias: '别　名',
+        };
         function createScoreHTML(score) {
             return (
                 `<i class="iconfont icon-star-full"></i>`.repeat(Math.floor(score)) +
@@ -19,37 +34,28 @@ export default function main() {
             );
         }
         function createInfoHTML(book) {
-            return Object.entries({
-                formerName: '原　名',
-                author: '作　者',
-                translator: '译　者',
-                press: '出版社',
-                year: '出版年',
-                scenarist: '编　剧',
-                star: '主　演',
-                type: '类　型',
-                productionCountry: '制片国家/地区',
-                language: '语　言',
-                releaseDate: '上映日期',
-                filmLength: '片　长',
-                alias: '别　名',
-            }).reduce((acc, [key, label]) => {
-                if (book[key]) acc += `<span title="${book[key]}">${label}：${book[key]}</span><br>`;
-                return acc;
-            }, '');
+            return Object.entries(templateData)
+                .reduce((acc, [key, label]) => {
+                    if (book[key]) acc.push(`<span title="${book[key]}">${label}：${book[key]}</span><br>`);
+                    return acc;
+                }, [])
+                .join('');
         }
 
         $.__config.bookList.forEach((list) => {
             if (list.title) html += `<h1 class="iconfont ${list.icon}">${list.title}</h1>`;
             html += '<div class="book-cards">';
             list.books.forEach((book) => {
-                const cardHtml = booksTemp
-                    .replace('cover', book.cover || '')
-                    .replace('name', book.name || '')
-                    .replace('readDate', book.readDate || '')
-                    .replace('readPercentage', book.readPercentage || '')
-                    .replace('scoreHtml', createScoreHTML(book.score))
-                    .replace('infoHtml', createInfoHTML(book));
+                const cardHtml = $.__tools.batchTempReplacement(booksTemp, [
+                    ['cover', books.cover || ''],
+                    ['name', books.name || ''],
+                    ['readDate', readDate],
+                    ['readDateStyle', readDateStyle],
+                    ['readPercentage', readPercentage],
+                    ['readPercentageStyle', readPercentageStyle],
+                    ['scoreHtml', createScoreHTML(book.score)],
+                    ['infoHtml', createInfoHTML(book)],
+                ]);
                 html += cardHtml;
             });
             html += '</div>';
