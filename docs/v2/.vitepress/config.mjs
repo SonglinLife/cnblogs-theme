c865ea8567b86e8a695c65841b9463613b9c2006
wangyang <i@oyo.cool>
2025-01-09 17:35:44 +0800

    chore(docs): update docs

--- docs/v2/.vitepress/config.mjs
+++ docs/v2/.vitepress/config.mjs
@@ -1,32 +1,32 @@
-import { defineConfig } from 'vitepress'
-import zhConfig from './locales/zh.config.js'
+import { defineConfig } from 'vitepress';
+import zhConfig from './locales/zh.config.js';
 import markdownItTaskLists from 'markdown-it-task-lists';
-
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "SMemory",
-  description: "Documents",
-  lastUpdated: true,
-  srcDir: './src',
-  locales: {
-    root: zhConfig,
-  },
-   head: [['link', { rel: 'icon', href: 'https://pic.imgdb.cn/item/676dfbe9d0e0a243d4eb1a0c.png' }]],
-  themeConfig: {
-    logo: 'https://pic.imgdb.cn/item/676ba098d0e0a243d4e9d23d.png',
-    search: {
-      provider: 'local',
+    base: '/cnblogs-theme/',
+    title: 'SMemory',
+    description: 'Documents',
+    lastUpdated: true,
+    srcDir: './src',
+    locales: {
+        root: zhConfig,
     },
-    socialLinks: [
-      { icon: 'refinedgithub', link: 'https://github.com/wangyang0210/cnblogs-theme' },
-      { icon: 'github', link: 'https://github.com/BNDong/Cnblogs-Theme-SimpleMemory' },
-    ]
-  },
-  markdown: {
-    lineNumbers: true,
-    config: (md) => {
-        md.use(markdownItTaskLists)
-    }
-  }
-})
+    head: [['link', { rel: 'icon', href: 'https://pic.imgdb.cn/item/676dfbe9d0e0a243d4eb1a0c.png' }]],
+    themeConfig: {
+        logo: 'https://pic.imgdb.cn/item/676ba098d0e0a243d4e9d23d.png',
+        search: {
+            provider: 'local',
+        },
+        socialLinks: [
+            { icon: 'refinedgithub', link: 'https://github.com/wangyang0210/cnblogs-theme' },
+            { icon: 'github', link: 'https://github.com/BNDong/Cnblogs-Theme-SimpleMemory' },
+        ],
+    },
+    markdown: {
+        lineNumbers: true,
+        config: (md) => {
+            md.use(markdownItTaskLists);
+        },
+    },
+});
