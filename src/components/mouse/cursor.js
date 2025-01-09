fcf9db507bb055840e0e5c19cc4258c897a82d55
wangyang <i@oyo.cool>
2025-01-09 16:09:05 +0800

    chore(cursor): cursor

---
+++ src/components/mouse/cursor.js
@@ -0,0 +1,3 @@
+export default function main(options) {
+    document.body.style.cursor = `url(${options.url || 'https://files.cnblogs.com/files/wangyang0210/normal.gif?t=1681261712'}), default`;
+}
